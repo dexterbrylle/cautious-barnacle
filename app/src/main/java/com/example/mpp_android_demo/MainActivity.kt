6610fd1a1d98351270f397d43638e8fad19c78f7
Dexter Brylle Matos <dexterbrylle@gmail.com>
2020-07-14 16:30:13 +0800

    Update android app

--- app/src/main/java/com/example/mpp_android_demo/MainActivity.kt
+++ app/src/main/java/com/example/mpp_android_demo/MainActivity.kt
@@ -8,33 +8,17 @@ import sample.hello
 import Accounts.AccountsResponse
 import Accounts.GetAccountsTask
 import Accounts.TestAccountsResponse
-import Accounts.TestGetAccountsTask
+import Presenter.AccountState
+import Presenter.AccountView
 import android.provider.Settings
 import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.coroutines.*
 
-abstract class MainActivity : AppCompatActivity(), CoroutineScope {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        try {
-            val getAccountsTask = TestGetAccountsTask()
-            val job = Job()
-            val coroutineScope: CoroutineScope = CoroutineScope(job + Dispatchers.IO)
-
-            coroutineScope.launch {
-                val result = withContext(Dispatchers.IO) { getAccountsTask.callGetAccountsService() }
-                when ((result as TestAccountsResponse).result) {
-                    "OK" -> {
-                        val response = result;
-                        textView2.setText(response.data[0].accountId.toString())
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            Toast.makeText(this, "$e.message", Toast.LENGTH_SHORT).show()
-        }
-
+abstract class MainActivity : AppCompatActivity(), CoroutineScope,  AccountView{
+    private val accountsList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.recycler_view_1)
     }
+
+    override fun showState(state: AccountState) {}
 }
